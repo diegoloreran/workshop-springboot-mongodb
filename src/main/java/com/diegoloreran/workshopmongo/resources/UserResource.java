f43c4c7fa846d6b5c37ea1eafde19a633663f1d1
Diego Loreran <diegoloreran@gmail.com>
2023-01-03 15:45:52 -0300

    Inserção de usuário com POST

--- src/main/java/com/diegoloreran/workshopmongo/resources/UserResource.java
+++ src/main/java/com/diegoloreran/workshopmongo/resources/UserResource.java
@@ -1,38 +1,41 @@
 package com.diegoloreran.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegoloreran.workshopmongo.domain.User;
 import com.diegoloreran.workshopmongo.dto.UserDTO;
 import com.diegoloreran.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value="/users")
 public class UserResource {
-	
 	@Autowired
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> findAll() {
+ 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
-	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		
+	@GetMapping("/{id}")
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+ 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/diegoloreran/workshopmongo/services/UserService.java
+++ src/main/java/com/diegoloreran/workshopmongo/services/UserService.java
@@ -7,20 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegoloreran.workshopmongo.domain.User;
+import com.diegoloreran.workshopmongo.dto.UserDTO;
 import com.diegoloreran.workshopmongo.repository.UserRepository;
 import com.diegoloreran.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
+
 	@Autowired
 	private UserRepository repo;
 	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
 
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

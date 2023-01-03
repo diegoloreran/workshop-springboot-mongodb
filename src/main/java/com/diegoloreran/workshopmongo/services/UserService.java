fc15e0424e474d27ccf97f0fc3eabd17390396fe
Diego Loreran <diegoloreran@gmail.com>
2023-01-03 14:56:25 -0300

    Obtendo um usuário por id

--- src/main/java/com/diegoloreran/workshopmongo/services/UserService.java
+++ src/main/java/com/diegoloreran/workshopmongo/services/UserService.java
@@ -1,20 +1,26 @@
 package com.diegoloreran.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegoloreran.workshopmongo.domain.User;
 import com.diegoloreran.workshopmongo.repository.UserRepository;
+import com.diegoloreran.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-
 	@Autowired
 	private UserRepository repo;
-
+	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
